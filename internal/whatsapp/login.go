6f153800641e29627a13b784bee2b74494786687
Sandy <sandypriyatna98@gmail.com>
2026-05-17 04:02:16 +0700

    fix: use pure-go glebarez/go-sqlite driver for CGO-less VPS deployment

--- internal/whatsapp/login.go
+++ internal/whatsapp/login.go
@@ -12,7 +12,7 @@ import (
 	"go.mau.fi/whatsmeow/store/sqlstore"
 	waLog "go.mau.fi/whatsmeow/util/log"
 
-	_ "github.com/mattn/go-sqlite3"
+	_ "github.com/glebarez/go-sqlite"
 )
 
 // Connect initializes WhatsApp client with sqlite-backed session persistence.
@@ -38,7 +38,7 @@ func Connect(dbPath string, log waLog.Logger) (*whatsmeow.Client, error) {
 
 	container, err := sqlstore.New(
 		ctx,
-		"sqlite3",
+		"sqlite",
 		"file:"+dbPath+"?_foreign_keys=on&_busy_timeout=5000",
 		log,
 	)
